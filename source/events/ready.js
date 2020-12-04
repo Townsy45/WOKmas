7172972d41696ee6259d9c0eeab512e9e487512d
EC2 Default User <townsy45@gmail.com>
2020-12-04 00:44:06 +0000

    Updated ready event to set the status to the remaining days left.

--- source/events/ready.js
+++ source/events/ready.js
@@ -1,8 +1,17 @@
 const utils = require('../lib/utils');
+const axios = require('axios');
 
 module.exports = async (bot) => {
+  
+  let status;
+  
+  try {
+    const req = await axios.get('https://christmas-days.anvil.app/_/api/get_days');
+    status = (req.data) ? req.data['Days to Christmas'] + ` days till Christmas!` : 'Merry WOKmas!';
+  } catch (err) { status = 'Merry WOKmas!' }
+
   await bot.user.setStatus(`dnd`);
-  await bot.user.setActivity(`my creation!`, { url: "https://www.twitch.tv/townsydaboss", type: "STREAMING" });
+  await bot.user.setActivity(status, { url: "https://www.twitch.tv/townsydaboss", type: "STREAMING" });
 
   await utils.log.info(`${bot.user.username} is ready and waiting!`);
 };
